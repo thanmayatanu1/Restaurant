399804b2c3d3475d0e2114cc53fbc9dc712146f4
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-07-05 11:23:06 +0530

    Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React , {Fragment} from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 
 function App()
@@ -7,6 +8,10 @@ function App()
   return  (
     <Fragment>
        <Header />
+       <main>
+        <Meals />
+
+       </main>
     </Fragment>
   );
 }
